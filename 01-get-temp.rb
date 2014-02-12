0b45629439976570f768539269e67c05be2f1719
Nicolas Ledez <github@ledez.net>
2014-02-12 13:00:20 +0100

    Extraction dans une méthode pour aller chercher sur l'Arduino

--- 01-get-temp.rb
+++ 01-get-temp.rb
@@ -3,13 +3,18 @@ require 'json'
 require 'net/http'
 
 class MoniteurTemperature < Sinatra::Base
-  get '/temperatures' do
-    url = URI.parse('http://192.168.2.177/')
+  def valeursSurArduino(uri)
+    url = URI.parse(uri)
     req = Net::HTTP::Get.new(url.path)
     res = Net::HTTP.start(url.host, url.port) {|http|
       http.request(req)
     }
-    temps_json = res.body
+    res.body
+  end
+
+  get '/temperatures' do
+    #temps_json = valeursSurArduino 'http://192.168.2.177/'
+    temps_json = '{ "0": 12, "1": 43, "2": 42, "3": 45, "4": 23, "5": 23, "-1": 0}'
     temps = JSON.parse(temps_json)
     temps.to_s
   end
