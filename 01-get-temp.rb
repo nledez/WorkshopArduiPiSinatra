6e17c6fe18f9845cdd01af9a701667abf078252b
Nicolas Ledez <github@ledez.net>
2014-02-12 13:37:56 +0100

    Remise en route de l'intérogation Arduino

--- 01-get-temp.rb
+++ 01-get-temp.rb
@@ -14,8 +14,8 @@ class MoniteurTemperature < Sinatra::Base
   end
 
   def prendreLaTemperature
-    #temps_json = valeursSurArduino 'http://192.168.2.177/'
-    temps_json = '{ "0": 12, "1": 43, "2": 42, "3": 45, "4": 23, "5": 23, "-1": 0}'
+    temps_json = valeursSurArduino 'http://192.168.2.177/'
+    #temps_json = '{ "0": 12, "1": 43, "2": 42, "3": 45, "4": 23, "5": 23, "-1": 0}'
     @@temps = JSON.parse(temps_json)
   end
 
