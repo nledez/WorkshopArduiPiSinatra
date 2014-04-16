99e95a7e5c4d08341fa8eba6b1db5cec9a7fc8f4
Nicolas Ledez <github@ledez.net>
2014-04-16 12:42:37 +0200

    Mise en commentaire de ce qui n'est pas testé

--- lib/home.rb
+++ lib/home.rb
@@ -27,9 +27,9 @@ class TemperatureServer < Sinatra::Base
   helpers do
   # a helper method to turn a string ID
   # representation into a BSON::ObjectId
-  def object_id val
-    BSON::ObjectId.from_string(val)
-  end
+  #def object_id val
+  #  BSON::ObjectId.from_string(val)
+  #end
 
   def document_by_id id
     id = object_id(id) if String === id
